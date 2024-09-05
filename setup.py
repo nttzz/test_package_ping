1365e6f2eb5531f8b39a5d8b4635051a1f2ea9aa
Nguyen Trung Thanh <111987844+nttzz@users.noreply.github.com>
2024-09-05 17:20:16 +0700

    setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,37 @@
 from setuptools import setup, find_packages
+import os
 
-setup(
-    name='ping_script_test_package',  # Name of your package
-    version='1.0.0',  # Version of your package
-    packages=find_packages(),  # Automatically find the packages
-    entry_points={
-        'console_scripts': [
-            'ping-script=ping_script.ping:main',  # Create a command-line tool called 'ping-script'
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = 'A simple ping script utility.'
+
+def main():
+    setup(
+        name='ping_script_test_package',
+        version='1.0.0',
+        packages=find_packages(),  # Tự động tìm và bao gồm tất cả các package con
+        entry_points={
+            'console_scripts': [
+                'ping-script=app_test.ping:main',  # Tạo công cụ dòng lệnh từ hàm main() trong ping.py
+            ],
+        },
+        author='ntt_zz',
+        author_email='thanhnguyentrung0910@gmail.com',
+        description='A simple ping script utility.',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/nttzz/test_package_ping.git',
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
         ],
-    },
-    author='ntt_zz',
-    author_email='thanhnguyentrung0910@gmail.com',
-    description='A simple ping script utility.',
-    long_description=open('README.md').read(),  # Read from README.md for long description (optional)
-    long_description_content_type='text/markdown',
-    url='https://github.com/nttzz/test_package_ping.git',  # URL to your project's repository
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',  # Specify the Python version requirement
-)
+        python_requires='>=3.6',
+    )
+
+if __name__ == '__main__':
+    main()
